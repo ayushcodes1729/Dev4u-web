fc2010bce74418a88ff7e45f34b28f9cd814f679
Ayush Kumar <ayushtiwari0803@gmail.com>
2025-01-04 17:04:04 +0530

    Routing Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./Components/Navbar"
+import MainLayout from "./Layouts/MainLayout"
+import Login from "./Pages/Login"
+import Profile from "./Pages/Profile"
+import HomePage from "./Pages/HomePage"
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout/>}>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/profile" element={<Profile/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
